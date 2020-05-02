fd92e28793bc59dd1a1600ef004ffe3baa722710
zrong <zrongzrong@gmail.com>
2020-05-02 14:40:17 +0800

    remove account/role/user

--- pyape/app/models/__init__.py
+++ pyape/app/models/__init__.py
@@ -9,6 +9,3 @@ pyape.app.models
 from .typeid import TypeID
 from .regional import Regional, init_regional, get_regional_config
 from .valueobject import ValueObject, init_valueobject
-from .account import Account, init_account
-from .user import make_user_model, init_user
-from .role import Role, UserRole, init_userrole
